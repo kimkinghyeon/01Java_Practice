fe6fb4c954e0f8fed9c50f9a8611ca035a274985
DESKTOP-QJ6CEUJ\jo21y <modan8199@gmail.com>
2024-06-03 01:01:13 +0900

    완

--- 01_java_practice-main/chap04-control-flow-statement-practice/src/com/ohgiraffers/level01/basic/Application01.java
+++ 01_java_practice-main/chap04-control-flow-statement-practice/src/com/ohgiraffers/level01/basic/Application01.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.level01.basic;
 
+import java.util.Scanner;
+
 public class Application01 {
 
     public static void main(String[] args) {
@@ -9,7 +11,18 @@ public class Application01 {
         /* Question */
 
         /* Answer 답변을 기재해주세요. */
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("정수를 입력");
+        int n = sc.nextInt();
+        if (n % 2 == 1) {
+            System.out.println("홀수");
+        } else {
+            System.out.println("짝수");
+        }
+
+
+        System.out.println();
     }
 
 }
---
+++ 01_java_practice-main/chap04-control-flow-statement-practice/src/com/ohgiraffers/looping/Application01.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.looping;
+
+public class Application01 {
+    public static void main(String[] args) {
+
+        Question test = new Question();
+
+        test.first();
+        test.first2();
+        test.guguDan();
+        test.first3();
+        test.first4();
+
+    }
+}
